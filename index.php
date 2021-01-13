6403821b10e1b67746a81ea6870eb95fc400d489
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-01-13 13:57:53 +0100

    #1 + #2 Skapat php-filer. I index.php är formulär skapat med action, metod, input och submit-button

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Naturreservatet</title>
+</head>
+<body>
+
+    <form action="./result.php" method="post">
+        <input placeholder="apa" type="number" name="monkey">
+        <input placeholder="giraff" type="number" name="giraffe">
+        <input placeholder="tiger" type="number" name="tiger">
+        <input placeholder="kokosnöt" type="number" name="coconut">
+
+        <button type="submit">Spara</button>
+    </form>
+    
+    
+</body>
+</html>
