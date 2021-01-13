5b4bc3c6887f1be3c154eaff02ba8fc7d4bc96ea
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-01-13 16:26:14 +0100

    #4 Klasser och superklass är skapade. Funktioner kommer separat.

--- result.php
+++ result.php
@@ -1,4 +1,44 @@
 <?php 
 
+abstract class Animal {
 
+    abstract protected function getsound();
+
+}
+
+
+class Monkey extends Animal {
+    function __construct($img, $sound) {
+        //parent::__construct($img, $sound)
+    }
+    
+    $this->        
+
+}
+
+class Giraffe extends Animal {
+    function __construct($img, $sound) {
+        //parent::__construct($img, $sound)
+    }
+    
+    $this->        
+
+}
+
+class Tiger extends Animal {
+    function __construct($img, $sound) {
+        //parent::__construct($img, $sound)
+    }
+    
+    $this->        
+}
+
+class Coconut {
+    function __construct($img) {
+       
+    }
+    
+    $this->        
+
+}
 ?>
